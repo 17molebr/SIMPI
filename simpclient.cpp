120b9973ca991f5362c9f0f1f6949ef9a8be701c
17molebr <38011756+17molebr@users.noreply.github.com>
2020-07-20 13:46:08 -0700

    Add files via upload

---
+++ simpclient.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <stdlib.h>
+#define PORT 8080 
+   
+int main(int argc, char *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *num = argv[1];
+    int numprocess = atoi(num);
+    int numother = numprocess % 4;
+    
+    int buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+    
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+    
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , &numother , sizeof(numother), 0 ); 
+    printf("Nums sent\n"); 
+    
+    return 0;
+} 
