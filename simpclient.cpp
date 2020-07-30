4205931d64e16f160afbdebf3a15334da5cc407e
17molebr <38011756+17molebr@users.noreply.github.com>
2020-07-30 13:05:15 -0700

    Update simpclient.cpp

--- simpclient.cpp
+++ simpclient.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     
     
     //Local Workstaion simpi run
-    //run_simpi(4); 
+    run_simpi(4); 
     int fullrun = 4;
     for(int i=0; i < fullWorkstations; i++){   
     
