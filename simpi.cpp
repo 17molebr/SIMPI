213d762d0f6d6f7e10150e7665696e9c16b8c562
Bryce Molesh <brycemolesh@Bryces-MacBook-Pro.local>
2020-08-21 13:24:41 -0700

    test

--- simpi.cpp
+++ simpi.cpp
@@ -48,6 +48,7 @@ void run_client(matrix m, int s){
     r = send(s, &end, sizeof(end), 0); 
     r = send(s, &xdim, sizeof(xdim), 0); 
     r = send(s, &ydim, sizeof(ydim), 0);
+    std::cout << m.arr[6*xdim + 4];
     for (int a = start; a < end; a++)
     {
         for (int b = 0; b < xdim; b++)
@@ -173,7 +174,7 @@ void new_connection(int sock, server s) {
                 std::cout << "\n";
                 for (int j = 0; j < ydim; j++)
                 {
-                    std::cout << std::fixed << std::setprecision(2) << temp[j + i * xdim];
+                    std::cout << std::fixed << std::setprecision(2) << temp[i + j * xdim];
                     std::cout << ", ";
                 }
             }
