37047aae8b83190ccb68114890d2397ea0da08ca
Bryce Molesh <brycemolesh@Bryces-MacBook-Pro.local>
2020-09-29 12:35:23 -0700

    50x50 test for 2

--- simpi.cpp
+++ simpi.cpp
@@ -1179,15 +1179,15 @@ matrix &matrix::multiply(matrix other)
         int Acol = get_y();
         int Brow = other.get_x();
         int Bcol = other.get_y();
-        int rpp = Bcol / 1;
+        int rpp = Bcol / 2;
         int start = rpp * parId;
         int end = start + rpp;
         main_simpi->set_start(start);
         main_simpi->set_end(start+(end-start) * tempForProcesses);
-        if (Arow % 1 != 0)
+        if (Arow % 2 != 0)
         {
 
-            int leftover = Arow % 1;
+            int leftover = Arow % 2;
             if (parId < leftover)
             {
 
